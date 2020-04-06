1b3da6cb2bf05cd8d0c146db15bdd5537f3377cc
isZengwen <42513777+isZengwen@users.noreply.github.com>
2020-04-06 19:35:10 +0800

    change

--- cr_1.py
+++ cr_1.py
@@ -9,3 +9,7 @@ project1 = api.projects.add('Project1')
 task1 = api.items.add('Task1', project_id=project1['id'])
 task2 = api.items.add('Task2', project_id=project1['id'])
 api.commit()
+
+#print(api.state['projects'])
+# project1 = api.projects.add('Project1')
+
